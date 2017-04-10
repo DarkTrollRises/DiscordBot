15f06947bbcf8912a4aa8dfb6f0daca1bc7f127c
Rik van Doorn <rikvandoorn@msn.com>
2017-04-10 18:33:28 +0200

    Missend bestand toevoegen m.b.v. gitignore

---
+++ DiscordBot/Log/LogHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace DiscordBot.Log
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Discord;
+
+    public static class LogHandler
+    {
+        public static Task Log(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+
+            if (log.Severity == LogSeverity.Critical || log.Severity == LogSeverity.Error ||
+                log.Severity == LogSeverity.Warning)
+            {
+                File.WriteAllText($"Log\\{DateTime.Now:yyMMddHHmmss}.txt", log.ToString());
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
